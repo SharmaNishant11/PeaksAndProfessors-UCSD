2a26720ac8089ca35a155480b929af3084c2311e
SharmaNishant11 <nis005@ucsd.edu>
2026-09-08 04:13:16 -0700

    added nodemon and a test endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import express from "express"; // need "type":"module" in pkg.json
 
 const app = express();
 
+//route 
+app.get("/api/home", (req, res) => {
+    res.status(200).send("you got 20 notes")
+})
+
 app.listen(5001, () => {
     console.log("SERVER STARTED ON PORT: 5001");
 })
